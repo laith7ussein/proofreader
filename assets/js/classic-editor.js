2586d9134e3c1ecfec3b3c1629ee36076f7b8dbb
laith7ussein <laith7ussein@gmail.com>
2023-01-18 15:46:03 +0300

    clear formatting before paste

--- assets/js/classic-editor.js
+++ assets/js/classic-editor.js
@@ -13,8 +13,8 @@ jQuery(($) => {
         html = html.replace(/(<([^>]+)>)/gi, "");
 
         // add dots
-        if ( dots ) {
-            if ( html.length > 50 && html[html.length-1] !== '.' && html[html.length-1] !== ':' && html[html.length-1] !== '؟' && html[html.length-1] !== '!' && html[html.length-1] !== '،' && html[html.length-1] !== '؛' && html[html.length-1] !== '?' ) {
+        if (dots) {
+            if (html.length > 50 && html[html.length - 1] !== '.' && html[html.length - 1] !== ':' && html[html.length - 1] !== '؟' && html[html.length - 1] !== '!' && html[html.length - 1] !== '،' && html[html.length - 1] !== '؛' && html[html.length - 1] !== '?') {
                 html = `${html}.`;
             }
         }
@@ -24,35 +24,35 @@ jQuery(($) => {
 
         // replace arabic numbers
         [
-            {'٠': '0'},
-            {'١': '1'},
-            {'٢': '2'},
-            {'٣': '3'},
-            {'٤': '4'},
-            {'٥': '5'},
-            {'٦': '6'},
-            {'٧': '7'},
-            {'٨': '8'},
-            {'٩': '9'},
-            {'٠': '0'},
-            {'١': '1'},
-            {'٢': '2'},
-            {'٣': '3'},
-            {'٤': '4'},
-            {'٥': '5'},
-            {'٦': '6'},
-            {'٧': '7'},
-            {'٨': '8'},
-            {'٩': '9'},
-            {'٪': '%'},
+            { '٠': '0' },
+            { '١': '1' },
+            { '٢': '2' },
+            { '٣': '3' },
+            { '٤': '4' },
+            { '٥': '5' },
+            { '٦': '6' },
+            { '٧': '7' },
+            { '٨': '8' },
+            { '٩': '9' },
+            { '٠': '0' },
+            { '١': '1' },
+            { '٢': '2' },
+            { '٣': '3' },
+            { '٤': '4' },
+            { '٥': '5' },
+            { '٦': '6' },
+            { '٧': '7' },
+            { '٨': '8' },
+            { '٩': '9' },
+            { '٪': '%' },
         ].forEach(function (obj) {
             Object.keys(obj).forEach(function (key) {
                 const value = obj[key];
                 html = html.replaceAll(key, value);
             });
         });
-        
-        
+
+
         html = html.replaceAll(/\.+$/g, '.');
         html = html.replaceAll(/(\s\و)\s/g, '$1');
         html = html.replaceAll(/([^0-9])([\،\,\.\؛\;\:\:])([!@#$%^&*()-_=+0-9a-zA-Zء-ي0]+)/g, '$1$2 $3');
@@ -66,19 +66,19 @@ jQuery(($) => {
         html = html.replaceAll(/(\()\s([a-zA-Z]+)/g, '$1$2');
         html = html.replaceAll(/(\))([a-zA-Zء-ي])/g, '$1 $2');
         html = html.replaceAll(/([a-zA-Zء-ي])(\()/g, '$1 $2');
-        
+
         // words
         if (typeof proofreader_words === typeof []) {
             Object.keys(proofreader_words).forEach(function (wordFull) {
                 const replacement = proofreader_words[wordFull];
                 const wordsSplitted = wordFull.split('-');
                 wordsSplitted.forEach(function (word) {
-                    
+
                     word = word.trim().replace(/([()])/g, '\\$1');
                     const regex = new RegExp(`([\\s\\و]|^)${word}([\\،\\s\\.\\,\\:\\؛\\;\\"\\(\\)\\'\\?\\!]|$)`, 'g');
-                    
+
                     html = html.replaceAll(regex, `$1${replacement}$2`);
-                    
+
                 });
             });
         } else alert('No words found');
@@ -90,7 +90,7 @@ jQuery(($) => {
             const regex = new RegExp(`([\\s\\و]|^)${word}([\\،\\s\\.\\,\\:\\؛\\;\\"\\(\\)\\'\\?\\!]|$)`, 'g');
             html = html.replaceAll(regex, `$1<strong>${word}</strong>$2`);
         });
-        
+
         // double quotes
         if (html.length) {
             let quotesCount = 0;
@@ -98,14 +98,14 @@ jQuery(($) => {
             newHtml.forEach(function (char, index) {
                 if (char === '"') {
                     quotesCount += 1;
-                    switch ( quotesCount % 2 ) {
+                    switch (quotesCount % 2) {
                         case 0:
                             if (html[index - 1] === ' ') newHtml[index - 1] = '';
-                            if (html[index + 1] !== ' ' && typeof html[index + 1] !== typeof undefined && html[index + 1] !== '.' && html[index + 1] !== ',' && html[index + 1] !== '،' && html[index + 1] !== ':' && html[index + 1] !== ';') newHtml[index + 1] = ' '+newHtml[index + 1];
+                            if (html[index + 1] !== ' ' && typeof html[index + 1] !== typeof undefined && html[index + 1] !== '.' && html[index + 1] !== ',' && html[index + 1] !== '،' && html[index + 1] !== ':' && html[index + 1] !== ';') newHtml[index + 1] = ' ' + newHtml[index + 1];
                             if (html[index + 1] === ' ' && typeof html[index + 2] !== typeof undefined && (html[index + 2] === '.' || html[index + 2] === ',' || html[index + 2] === '،' || html[index + 2] === ':' || html[index + 2] === ';')) newHtml[index + 1] = '';
                             break;
                         case 1:
-                            if (typeof html[index - 1] !== typeof undefined && html[index - 1] !== ' ' && html[index - 1] !== 'و' && html[index - 1] !== 'ـ') newHtml[index - 1] = newHtml[index - 1]+' ';
+                            if (typeof html[index - 1] !== typeof undefined && html[index - 1] !== ' ' && html[index - 1] !== 'و' && html[index - 1] !== 'ـ') newHtml[index - 1] = newHtml[index - 1] + ' ';
                             if (html[index + 1] === ' ') newHtml[index + 1] = '';
                     }
                 }
@@ -117,8 +117,39 @@ jQuery(($) => {
 
     }
 
+    tinymce.init({
+        selector: 'textarea',  // change this value according to your HTML
+        plugins: 'paste',
+        menubar: 'edit',
+        toolbar: 'paste',
+        paste_webkit_styles: ''
+    });
+
     tinymce.PluginManager.add('proofreader_mce_button', function (editor, url) {
-        console.log(editor);
+
+        // on paste
+        editor.on('PastePreProcess', function (e) {
+            // get plain text from clipboard
+            let text = e.content;
+
+            // remove span, a, strong and em tags
+            text = text.replace(/<(span|a|strong|em)[^>]*>/g, '');
+            text = text.replace(/<\/(span|a|strong|em)>/g, '');
+
+            text = text.replace(/(<([^>]+)>)/ig, '<br>');
+
+            // remove multiple <br>
+            text = text.replace(/(<br>\\*)+/g, '<br>');
+
+            // convert <br> to <p>
+            text = text.replace(/<br>/g, '</p><p>');
+
+            e.content = text;
+            e.content = '';
+
+        });
+
+
         editor.addButton('proofreader_mce_button', {
             onclick: function () {
 
@@ -171,5 +202,6 @@ jQuery(($) => {
             tooltip: 'تدقيق النص',
         });
     });
+
 
 }, 'jquery');
